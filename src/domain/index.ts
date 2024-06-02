e866eb29058d8aed10a1a7064695b0aff7dbe5b2
DemianHoftm <Thegodneko@gmail.com>
2024-06-01 22:46:30 -0400

    arreglos del merge

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -3,3 +3,5 @@ export * from "./datasource/diocese.datasource";
 export * from "./entities/diocese.entity";
 export * from "./repositories/diocese.repository";
 export * from "./useCases"
+export * from "./repositories/role_repository";
+export * from "./datasource/role_datasource";
