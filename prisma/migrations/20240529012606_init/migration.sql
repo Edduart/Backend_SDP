402bbb95fce62e56a0ffa7a98d15ad792c88438a
edduart <edduar13@gmail.com>
2024-06-02 12:12:34 -0400

    comment unnecesary

--- prisma/migrations/20240529012606_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Diocese" (
-    "id" SERIAL NOT NULL,
-    "name" VARCHAR NOT NULL,
-    "holder" VARCHAR NOT NULL,
-
-    CONSTRAINT "Diocese_pkey" PRIMARY KEY ("id")
-);
--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,6 +3,6 @@ import { get } from "env-var";
 
 export const envs = {
   PORT: get("PORT").required().asPortNumber(),
-  PUBLIC_PATH: get("PUBLIC_PATH").default("public").asString(),
-  MYSQL_URL: get("MYSQL_URL").default("public").asString(),
+  /*PUBLIC_PATH: get("PUBLIC_PATH").default("public").asString(),
+  MYSQL_URL: get("MYSQL_URL").default("public").asString(),*/
 };
