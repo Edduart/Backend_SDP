37e125f49e040e69ebc057558b114f44d9aca118
José Carrillo <josecarrillom568@gmail.com>
2024-06-02 22:27:11 -0400

    xdd

---
+++ src/domain/useCases/parish/usecase.getParishes.ts
@@ -0,0 +1,14 @@
+import { DioceseEntity } from "../../entities/diocese.entity";
+import { DioceseRepository } from "../../repositories/diocese.repository";
+
+export interface GetDiocesesUseCase {
+  execute(): Promise<DioceseEntity[]>;
+}
+
+export class GetDioceses implements GetDiocesesUseCase {
+  constructor(private readonly repository: DioceseRepository) {}
+
+  execute(): Promise<DioceseEntity[]> {
+    return this.repository.getAll();
+  }
+}
