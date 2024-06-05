739f653142cdd4512bbfbdc63745c81281e6a7b3
edduart <edduar13@gmail.com>
2024-06-05 12:22:13 -0400

    fix: remove unecessary CRUD options

--- src/domain/datasource/stage.datasource.ts
+++ src/domain/datasource/stage.datasource.ts
@@ -1,10 +1,6 @@
 import { StageEntity } from "../entities";
-import { CreateStageDto, UpdateStageDto } from "../dtos/";
 
 export abstract class StageDataSource {
-  abstract create(dto: CreateStageDto): Promise<StageEntity>;
-  abstract updateById(dto: UpdateStageDto): Promise<StageEntity>;
   abstract getAll(): Promise<StageEntity[]>;
   abstract findById(id: number): Promise<StageEntity>;
-  abstract deleteById(id: number): Promise<StageEntity>;
 }
--- src/infrastructure/repositories/stage.repository.imple.ts
+++ src/infrastructure/repositories/stage.repository.imple.ts
@@ -1,21 +1,13 @@
-import { CreateStageDto, StageDataSource, StageEntity, StageRepository, UpdateStageDto } from "../../domain";
+import { StageDataSource, StageEntity, StageRepository } from "../../domain";
 
 
 export class StageRepositoryImpl implements StageRepository {
   constructor(private readonly datasource: StageDataSource) {}
-  create(dto: CreateStageDto): Promise<StageEntity> {
-    return this.datasource.create(dto);
-  }
+
   getAll(): Promise<StageEntity[]> {
     return this.datasource.getAll();
   }
   findById(id: number): Promise<StageEntity> {
     return this.datasource.findById(id);
-  }
-  updateById(dto: UpdateStageDto): Promise<StageEntity> {
-    return this.datasource.updateById(dto);
-  }
-  delete(id: number): Promise<StageEntity> {
-    return this.datasource.deleteById(id);
   }
 }
