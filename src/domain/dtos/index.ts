b3f647ddba9933f74900323fff0db58425e52284
edduart <edduar13@gmail.com>
2024-06-05 15:27:31 -0400

    fix: remove stage dto rutes

--- src/domain/dtos/index.ts
+++ src/domain/dtos/index.ts
@@ -4,8 +4,7 @@ export * from "./diocese/getDioceseByName.dto"
 
 export * from './role/create.role';
 export * from './role/update.role';
-export * from "./stage/createStage"
-export * from "./stage/updateStage";
+
 export * from "./phone/phone.create"
 export * from "./socialmedia/socialmedia.create"
 export * from "./worker/create.worker"
