bef8de92e716054231ba1f67944cf878545ffe3a
edduart <edduar13@gmail.com>
2024-06-18 10:35:11 -0400

    shore: fix and add seeders

--- src/seed/data/dioceses.ts
+++ src/seed/data/dioceses.ts
@@ -1,145 +1,181 @@
 export const dioceses = [
   {
+    id: 1,
     name: "Arquidiócesis de Barquisimeto",
     holder: "Monseñor. Víctor Hugo Basabe",
   },
   {
+    id: 2,
     name: "Arquidiócesis de Bolívar ",
     holder: "Monseñor. Ulises Antonio Gutiérrez Reyes",
   },
   {
+    id: 3,
     name: "Arquidiócesis de Calabozo",
     holder: "Monseñor. Manuel Felipe Díaz",
   },
   {
+    id: 4,
     name: "Arquidiócesis de Caracas",
     holder: "Cardenal. Baltazar Enrique Porras Cardozo",
   },
   {
+    id: 5,
     name: "Arquidiócesis de Coro",
     holder: "Monseñor. Mariano Parra",
   },
   {
+    id: 6,
     name: "Arquidiócesis de Cumaná",
     holder: "Monseñor. Jesús González De Zárate",
   },
   {
+    id: 7,
     name: "Arquidiócesis de Maracaibo",
     holder: "Monseñor. José Luis Azuaje Ayala",
   },
   {
+    id: 8,
     name: "Arquidiócesis de Mérida",
     holder: "Emmo. Cardenal. Baltazar Porras Cardozo",
   },
   {
+    id: 9,
     name: "Arquidiócesis de Valencia ",
     holder: "Monseñor. Saúl Figueroa Albornoz",
   },
   {
+    id: 10,
     name: "Diócesis de Acarigua",
     holder: "Monseñor. Gerardo Ernesto Salas Arjona",
   },
   {
+    id: 11,
     name: "Diócesis de Barcelona",
     holder: "Monseñor. Jorge Aníbal Quintero",
   },
   {
+    id: 12,
     name: "Diócesis de Barinas",
     holder: "Monseñor. Alfonso Guerrero Contreras",
   },
   {
+    id: 13,
     name: "Diócesis de Cabimas",
     holder: "Monseñor. Ángel Francisco Caraballo Fermín",
   },
   {
+    id: 14,
     name: "Diócesis de Carora",
     holder: "Monseñor. Carlos Enrique Curiel Herrera",
   },
   {
+    id: 15,
     name: "Diócesis de Carúpano",
     holder: "Monseñor. Jaime Villarroel Rodríguez",
   },
   {
+    id: 16,
     name: "Diócesis de Ciudad Guayana",
     holder: "Obispo. Presbítero. Jorge Aníbal Quintero",
   },
   {
+    id: 17,
     name: "Diócesis de El Tigre",
     holder: "Monseñor. José Manuel Romero Barrios ",
   },
   {
+    id: 18,
     name: "Diócesis de El Vigía - San Carlos del Zulia",
     holder: "Monseñor. Juan de Dios Peña",
   },
   {
+    id: 19,
     name: "Diócesis de Guanare",
     holder: "Monseñor. José de la Trinidad Valera Angulo",
   },
   {
+    id: 20,
     name: "Diócesis de Guarenas",
     holder: "Monseñor. Tulio Luis Ramírez Padilla",
   },
   {
+    id: 21,
     name: "Diócesis de Guasdualito",
     holder: "Monseñor. Pablo Modesto González Pérez",
   },
   {
+    id: 22,
     name: "Diócesis de La Guaira",
     holder: "Monseñor. Raúl Biord Castillo",
   },
   {
+    id: 23,
     name: "Diócesis de Los Teques",
     holder: "Monseñor. Freddy Fuenmayor Suárez",
   },
   {
+    id: 24,
     name: "Diócesis de Machiques",
     holder: "Monseñor. Nicolás Gregorio Nava Rojas",
   },
   {
+    id: 25,
     name: "Diócesis de Maracay",
     holder: "Monseñor. Enrique José Parravano Marino",
   },
   {
+    id: 26,
     name: "Diócesis de Margarita",
     holder: "Monseñor. Fernando Castro Aguayo",
   },
   {
+    id: 27,
     name: "Diócesis de Maturín",
     holder: "Monseñor. Enrique Pérez Lavado",
   },
   {
+    id: 28,
     name: "Diócesis de Petare",
     holder: "Monseñor. Juan Carlos Bravo Salazar",
   },
   {
+    id: 29,
     name: "Diócesis de Puerto Cabello",
     holder: "Monseñor. Saúl Figueroa Albornoz",
   },
   {
+    id: 30,
     name: "Diócesis de Punto Fijo",
     holder: "Monseñor. Carlos Alfredo Cabezas Mendoza",
   },
   {
+    id: 31,
     name: "Diócesis de San Carlos",
     holder: "Monseñor. Polito Rodríguez Méndez",
   },
   {
+    id: 32,
     name: "Diócesis de San Cristóbal",
     holder: "Monseñor. Mario del Valle Moronta Rodríguez",
   },
   {
+    id: 33,
     name: "Diócesis de San Felipe",
     holder: "Monseñor. Víctor Hugo Basabe",
   },
   {
+    id: 34,
     name: "Diócesis de San Fernando de Apure",
     holder: "Monseñor. Alfredo Enrique Torres Rondón",
   },
   {
+    id: 35,
     name: "Diócesis de Trujillo",
     holder: "Monseñor. José Trinidad Valera Angulo",
   },
   {
+    id: 36,
     name: "Diócesis de Valle de la Pascua",
     holder: "Monseñor. Ramón José Aponte Fernández",
   },
--- src/seed/data/parish.ts
+++ src/seed/data/parish.ts
@@ -1,81 +1,97 @@
-
-
-export const parishes = [{
-    diocese_id:1,
+export const parishes = [
+  {
+    id: 1,
+    diocese_id: 1,
     name: "BASÍLICA MENOR SANTO CRISTO DE LA GRACIA",
-    patron: "SANTÍSIMO SACRAMENTO"
-},
-{
-    diocese_id:2,
+    patron: "SANTÍSIMO SACRAMENTO",
+  },
+  {
+    id: 2,
+    diocese_id: 2,
     name: "BEATA MARÍA DE SAN JOSÉ",
-    patron: "EL NAZARENO"
-},
-{
-    diocese_id:3,
+    patron: "EL NAZARENO",
+  },
+  {
+    id: 3,
+    diocese_id: 3,
     name: "CRISTO REDENTOR",
-    patron: "NUESTRA SEÑORA DE COROMOTO"
-},
-{
-    diocese_id:4,
+    patron: "NUESTRA SEÑORA DE COROMOTO",
+  },
+  {
+    id: 4,
+    diocese_id: 4,
     name: "CRISTO REY",
-    patron: "SANTOS APÓSTOLES"
-},
-{
-    diocese_id:5,
+    patron: "SANTOS APÓSTOLES",
+  },
+  {
+    id: 5,
+    diocese_id: 5,
     name: "DIVINA PASTORA",
-    patron: "SANTÍSIMO SACRAMENTO"
-},
-{
-    diocese_id:6,
+    patron: "SANTÍSIMO SACRAMENTO",
+  },
+  {
+    id: 6,
+    diocese_id: 6,
     name: "DIVINO NIÑO (EL TRIGAL)",
-    patron: "SAGRADO CORAZÓN DE JESÚS"
-},
-{
-    diocese_id:7,
+    patron: "SAGRADO CORAZÓN DE JESÚS",
+  },
+  {
+    id: 7,
+    diocese_id: 7,
     name: "EL SALVADOR",
-    patron: "NUESTRA SEÑORA DE COROMOTO"
-},
-{
-    diocese_id:8,
+    patron: "NUESTRA SEÑORA DE COROMOTO",
+  },
+  {
+    id: 8,
+    diocese_id: 8,
     name: "ESPÍRITU SANTO (BQTO)",
-    patron: "ESPÍRITU SANTO"
-},{
-    diocese_id:9,
+    patron: "ESPÍRITU SANTO",
+  },
+  {
+    id: 9,
+    diocese_id: 9,
     name: "INMACULADA CONCEPCIÓN (BUENA VISTA)",
-    patron: "NTRA SRA DE ALTAGRACIA"
-},
-{
-    diocese_id:10,
+    patron: "NTRA SRA DE ALTAGRACIA",
+  },
+  {
+    id: 10,
+    diocese_id: 10,
     name: "INMACULADA CONCEPCIÓN (CUARA)",
-    patron: "NTRA SRA DE ALTAGRACIA"
-},
-{
-    diocese_id:11,
+    patron: "NTRA SRA DE ALTAGRACIA",
+  },
+  {
+    id: 11,
+    diocese_id: 11,
     name: "JESÚS DE NAZARET",
-    patron: "SANTÍSIMA TRINIDAD"
-},
-{
-    diocese_id:12,
+    patron: "SANTÍSIMA TRINIDAD",
+  },
+  {
+    id: 12,
+    diocese_id: 12,
     name: "LA MEDALLA MILAGROSA",
-    patron: "SANTÍSIMO SACRAMENTO"
-},
-{
-    diocese_id:13,
+    patron: "SANTÍSIMO SACRAMENTO",
+  },
+  {
+    diocese_id: 13,
     name: "LA RESURRECCIÓN DEL SEÑOR",
-    patron: "SAN JOSÉ"
-},
-{
-    diocese_id:14,
+    patron: "SAN JOSÉ",
+  },
+  {
+    id: 14,
+    diocese_id: 14,
     name: "MARÍA AUXILIADORA",
-    patron: "ESPÍRITU SANTO"
-},
-{
-    diocese_id:15,
+    patron: "ESPÍRITU SANTO",
+  },
+  {
+    id: 15,
+    diocese_id: 15,
     name: "MARÍA AUXILIADORA (LA MIEL)",
-    patron: "EL NAZARENO"
-},
-{
-    diocese_id:16,
+    patron: "EL NAZARENO",
+  },
+  {
+    id: 16,
+    diocese_id: 16,
     name: "MARÍA REINA DE TODOS LOS SANTOS",
-    patron: "NUESTRA SEÑORA DE COROMOTO"
-},];
+    patron: "NUESTRA SEÑORA DE COROMOTO",
+  },
+];
