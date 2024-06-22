df63fbb11b5dbebf5bb07d8cb684f7050c8b91a7
DemianHoftm <Thegodneko@gmail.com>
2024-06-22 18:39:01 -0400

    todo funcional

---
+++ src/presentation/routes/extras.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { ValidatorTo } from "../services/TokenValidator";
+import { ExtraController } from "../extra/extra.controller";
+
+const router = Router();
+const ExtraControl = new ExtraController();
+
+
+router.get("/blood/", ValidatorTo.ValidarToken, ExtraControl.Blood);
+router.get("/work/",ValidatorTo.ValidarToken, ExtraControl.Work);
+module.exports= router;
