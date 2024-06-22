e16ee5bf2f0da3f1973685f863125827fce0fae8
DemianHoftm <Thegodneko@gmail.com>
2024-06-22 17:32:40 -0400

    implementacion completada de tokens

--- src/presentation/routes/diocese.ts
+++ src/presentation/routes/diocese.ts
@@ -2,17 +2,18 @@ import { Router } from "express";
 import { DioceseController } from "../diocese/diocese.controller";
 import { DioceseDataSourceImpl } from "../../infrastructure/datasource/";
 import { DioceseRepositoryImpl } from "../../infrastructure/repositories/";
+import { ValidatorTo } from "../services/TokenValidator";
 
 const router = Router();
 const datasource = new DioceseDataSourceImpl();
 const dioceseRepository = new DioceseRepositoryImpl(datasource);
 const dioceseController = new DioceseController(dioceseRepository);
 
-router.post("/", dioceseController.createDiocese);
-router.get("/", dioceseController.getDioceses);
-router.get("/search", dioceseController.getDioceseByName);
-router.get("/:id", dioceseController.getDioceseById);
-router.put("/:id", dioceseController.updateDioceseById);
-router.delete("/:id", dioceseController.deleteDiocese);
+router.post("/", ValidatorTo.ValidarToken, dioceseController.createDiocese);
+router.get("/", ValidatorTo.ValidarToken, dioceseController.getDioceses);
+router.get("/search", ValidatorTo.ValidarToken, dioceseController.getDioceseByName);
+router.get("/:id", ValidatorTo.ValidarToken, dioceseController.getDioceseById);
+router.put("/:id", ValidatorTo.ValidarToken, dioceseController.updateDioceseById);
+router.delete("/:id", ValidatorTo.ValidarToken, dioceseController.deleteDiocese);
 module.exports = router;
 
