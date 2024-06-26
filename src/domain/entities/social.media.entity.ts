f321d026b6c16587f0db623a8c866f4b7a23f3ec
edduart <edduar13@gmail.com>
2024-06-26 16:14:37 -0400

    fix: improve in verbosity of code in professor datasource

--- src/domain/entities/social.media.entity.ts
+++ src/domain/entities/social.media.entity.ts
@@ -1,17 +1,13 @@
-export class SocialMediaEntity{
-    constructor(
-        public id: number| null,
-        public person_id: string| null,
-        public social_Cate: string,
-        public link: string,
-    ){}
-    
-    public static fromdb(object: {[key: string]: any}){
-        const {id, person_id, social_Cate, link} = object;
-        return new SocialMediaEntity(id, person_id, social_Cate, link)
-    }
+export class SocialMediaEntity {
+  constructor(
+    public id: number | null,
+    public person_id: string | null,
+    public social_media_category: string,
+    public link: string
+  ) {}
 
-
-
-
+  public static fromdb(object: { [key: string]: any }) {
+    const { id, person_id, social_media_category, link } = object;
+    return new SocialMediaEntity(id, person_id, social_media_category, link);
+  }
 }
--- src/infrastructure/datasource/professor.datasource.impl.ts
+++ src/infrastructure/datasource/professor.datasource.impl.ts
@@ -1,5 +1,6 @@
-import { person_BloodType } from "@prisma/client";
+import { person_BloodType, social_media } from "@prisma/client";
 import { prisma } from "../../data/postgres";
+import { SocialMedia } from "../../domain/dtos/socialmedia/socialmedia.create";
 import {
   BloodType,
   CreateProfessor,
@@ -12,103 +13,67 @@ import {
 
 export class ProfessorDataSourceImpl implements ProfessorDataSource {
   async create(createDto: CreateProfessor): Promise<ProfessorEntity> {
-    try {
-      prisma.$transaction(async () => {
-        const createPerson = await prisma.person.create({
-          data: {
-            id: createDto.persona.id,
-            forename: createDto.persona.forename,
-            surname: createDto.persona.surname,
-            birthdate: createDto.persona.birthdate,
-            profile_picture_path: createDto.persona.profile_picture_path,
-            email: createDto.persona.email,
-            medical_record: createDto.persona.medical_record,
-            BloodType: createDto.persona.BloodType as person_BloodType,
-          },
-        });
-        if (createDto.social != null) {
-          const dataSocialMedia = createDto.social.map((social) => {
-            return {
-              person_id: createPerson.id,
-              social_media_category: social.social_media_category,
-              link: social.link,
-            };
-          });
-          await prisma.social_media.createMany({
-            data: dataSocialMedia,
-          });
-        }
-        if (createDto.telefono != null) {
-          const dataCellPhone = createDto.telefono.map((cellPhone) => {
-            return {
-              person_id: createPerson.id,
-              phone_number: cellPhone.phone_numbre,
-              description: cellPhone.description,
-            };
-          });
-          await prisma.phone_number.createMany({
-            data: dataCellPhone,
-          });
-        }
-        await prisma.professor.create({
-          data: {
-            id: createPerson.id,
-            status_id: 1,
-          },
-        });
-        const resultIndividual = await this.get(
-          createPerson.id,
-          undefined
-        );
-        return resultIndividual[0];
+    prisma.$transaction(async () => {
+      const createPerson = await prisma.person.create({
+        data: {
+          id: createDto.persona.id,
+          forename: createDto.persona.forename,
+          surname: createDto.persona.surname,
+          birthdate: createDto.persona.birthdate,
+          profile_picture_path: createDto.persona.profile_picture_path,
+          email: createDto.persona.email,
+          medical_record: createDto.persona.medical_record,
+          BloodType: createDto.persona.BloodType as person_BloodType,
+        },
       });
-    } catch (error: any) {
-      throw new Error("Something went wrong" + error);
-    }
-    const resultIndividual = await this.get(createDto.persona.id, undefined);
+      if (createDto.social != null) {
+        const dataSocialMedia = createDto.social.map((social) => {
+          return {
+            person_id: createPerson.id,
+            social_media_category: social.social_media_category,
+            link: social.link,
+          };
+        });
+        await prisma.social_media.createMany({
+          data: dataSocialMedia,
+        });
+      }
+      if (createDto.telefono != null) {
+        const dataCellPhone = createDto.telefono.map((cellPhone) => {
+          return {
+            person_id: createPerson.id,
+            phone_number: cellPhone.phone_numbre,
+            description: cellPhone.description,
+          };
+        });
+        await prisma.phone_number.createMany({
+          data: dataCellPhone,
+        });
+      }
+      await prisma.professor.create({
+        data: {
+          id: createPerson.id,
+          status_id: 1,
+        },
+      });
+    });
+    const resultIndividual = await this.get(createDto.persona.id, undefined); //this can be improved => check in future
     return resultIndividual[0];
   }
 
-  async get(
-    id: string | undefined,
-    status_id: number | undefined,
-  ): Promise<ProfessorEntity[]> {
-    /* si ambas variables son undefined se procede con el select all, sin embargo si una de las variables es undefined
-      se procede con el filtro, pues prisma controla el undefined como una forma de ignorar dicha comparacion 
-      y null como un valor */
+  async get(id?: string, status_id?: number): Promise<ProfessorEntity[]> {
     let retunrFromDB;
-    if (id === undefined && status_id === undefined) {
+    if (!id && !status_id) {
+      console.log("going for all");
       retunrFromDB = await prisma.person.findMany({
-        select: {
-          id: true,
-          profile_picture_path: true,
-          forename: true,
-          surname: true,
-          email: true,
-          birthdate: true,
-          medical_record: true,
-          BloodType: true,
-          phone_number: {
-            select: {
-              phone_number: true,
-              description: true,
-            },
-          },
+        include: {
+          phone_number: true,
+          professor: true,
           social_media: {
-            select: {
-              link: true,
-              //Esta es la llave de relacion generada por prisma automaticamente, por ello el nombre extraño, por favor ignorarlo
+            
+            include: {
               social_media_category_social_media_social_media_categoryTosocial_media_category:
-                {
-                  select: {
-                    description: true,
-                  },
-                },
-            },
-          },
-          professor: {
-            select: {
-              status_id: true,
+                { select: { description: true } },
             },
           },
         },
@@ -125,81 +90,34 @@ export class ProfessorDataSourceImpl implements ProfessorDataSource {
             },
           ],
         },
-        select: {
-          id: true,
-          profile_picture_path: true,
-          forename: true,
-          surname: true,
-          email: true,
-          birthdate: true,
-          medical_record: true,
-          BloodType: true,
-          phone_number: {
-            select: {
-              phone_number: true,
-              description: true,
-            },
-          },
+        include: {
+          phone_number: true,
+          professor: true,
           social_media: {
-            select: {
-              link: true,
-              //Esta es la llave de relacion generada por prisma automaticamente, por ello el nombre extraño, por favor ignorarlo
+            include: {
               social_media_category_social_media_social_media_categoryTosocial_media_category:
-                {
-                  select: {
-                    description: true,
-                  },
-                },
-            },
-          },
-          professor: {
-            select: {
-              status_id: true,
+                { select: { description: true } },
             },
           },
         },
       });
     }
-
-    const workers: ProfessorEntity[] = retunrFromDB.map((professor) => {
-      //creo a la persona otra cosa, worker es basicamente persona debido a que el select empezó por persona
-      const person: PersonEntity = PersonEntity.fromdb({
-        id: professor.id,
-        profile_picture_path: professor.profile_picture_path,
-        forename: professor.forename,
-        surname: professor.surname,
-        email: professor.email,
-        fecha: professor.birthdate,
-        medical_record: professor.medical_record,
-        BloodType: professor.BloodType as BloodType,
-      });
+    //mapeo de los datos
+    const professors: ProfessorEntity[] = retunrFromDB.map((professor) => {
+      const person: PersonEntity = PersonEntity.fromdb(professor); //datos de persona
       const phones: PhoneEntity[] = professor.phone_number.map(
         (phoneatributer) => {
-          //creo los arrays de telefonos, en caso de que existan
-          return PhoneEntity.fromdb({
-            id: null,
-            phone_number: phoneatributer.phone_number,
-            person_id: null,
-            description: phoneatributer.description,
-          });
+          return PhoneEntity.fromdb(phoneatributer);
         }
       );
       const socials: SocialMediaEntity[] = professor.social_media.map(
         (sociales) => {
-          //ahora creo el array de redes sociales, en caso de que existan
           return SocialMediaEntity.fromdb({
-            id: null,
-            person_id: null,
-            //esta tira es debido al nombre de la relacion
-            social_Cate:
-              sociales
-                .social_media_category_social_media_social_media_categoryTosocial_media_category
-                .description,
             link: sociales.link,
+            social_media_category: sociales.social_media_category_social_media_social_media_categoryTosocial_media_category
           });
         }
       );
-      //ahora si a crear la entidad trabajador
       return ProfessorEntity.fromObject(
         person,
         socials,
@@ -207,7 +125,6 @@ export class ProfessorDataSourceImpl implements ProfessorDataSource {
         professor.professor?.status_id! // check this status must be !null, never
       );
     });
-
-    return workers;
+    return professors;
   }
 }
