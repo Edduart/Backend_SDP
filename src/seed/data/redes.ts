7e4da7d4d9b62f4ae3282bec444046be88ba7eea
DemianHoftm <Thegodneko@gmail.com>
2024-07-09 15:05:49 -0400

    seeders fix

--- src/seed/data/redes.ts
+++ src/seed/data/redes.ts
@@ -2,57 +2,57 @@ export const redes = [
   {
     id: 1,
     description: "Facebook",
-    icon: "localhost:3000/images/icons/facebook.png",
+    icon: "localhost:3000/images/icons/facebook.svg",
   },
   {
     id: 2,
     description: "X (Twitter)",
-    icon: "localhost:3000/images/icons/x.png",
+    icon: "localhost:3000/images/icons/x.svg",
   },
   {
     id: 3,
     description: "LinkedIn",
-    icon: "localhost:3000/images/icons/link.png",
+    icon: "localhost:3000/images/icons/link.svg",
   },
   {
     id: 4,
     description: "GitHub",
-    icon: "localhost:3000/images/icons/github.png",
+    icon: "localhost:3000/images/icons/github.svg",
   },
   {
     id: 5,
     description: "YouTube",
-    icon: "localhost:3000/images/icons/youtube.png",
+    icon: "localhost:3000/images/icons/youtube.svg",
   },
   {
     id: 6,
     description: "Tiktok",
-    icon: "localhost:3000/images/icons/tiktok.png",
+    icon: "localhost:3000/images/icons/tiktok.svg",
   },
   {
     id: 7,
     description: "Pinterest",
-    icon: "localhost:3000/images/icons/pinte.png",
+    icon: "localhost:3000/images/icons/pinte.svg",
   },
   {
     id: 8,
     description: "Threads",
-    icon: "localhost:3000/images/icons/threa.png",
+    icon: "localhost:3000/images/icons/threa.svg",
   },
   {
     id: 9,
     description: "Instagram",
-    icon: "localhost:3000/images/icons/insta.png",
+    icon: "localhost:3000/images/icons/insta.svg",
   },
   {
     id: 10,
     description: "Twitch",
-    icon: "localhost:3000/images/icons/twit.png",
+    icon: "localhost:3000/images/icons/twit.svg",
   },
   {
     id: 11,
     description: "Discord",
-    icon: "localhost:3000/images/icons/discord.png",
+    icon: "localhost:3000/images/icons/discord.svg",
   },
 ];
 
