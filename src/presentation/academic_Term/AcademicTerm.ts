0863c08ba1f28c613384cfdcf0b89e2da3b2611b
DemianHoftm <Thegodneko@gmail.com>
2024-07-26 15:38:02 -0400

    supposed fix

--- src/presentation/academic_Term/AcademicTerm.ts
+++ src/presentation/academic_Term/AcademicTerm.ts
@@ -7,55 +7,55 @@ export class AcademicTermController {
         const [error_Arr, academicCreateDTO] = CreateAcademicTerm.create(req.body);
         if (academicCreateDTO != undefined) {
             new CreateAcademicTermUseCase(this.repository).execute(academicCreateDTO)
-            .then((academic)=>{res.status(200).json(academic).send();})
+            .then((academic)=>{ return res.status(200).json(academic).send();})
             .catch((error) => {
                 console.log(error);
-                res.status(400).json("Error creando periodos" + error).send()})
+                return res.status(400).json("Error creando periodos" + error).send()})
         }else{
             console.log(error_Arr);
-            res.status(400).json({ error_Arr }).send();
+            return res.status(400).json({ error_Arr }).send();
         } 
     }
     public Get = (req: Request, res: Response) => {
         const  get_dto = GetAcademicTerm.create(req.query);
         new GetAcademicTermUseCase(this.repository).execute(get_dto).then((results) => {
-            res.status(200).json(results).send();
+            return res.status(200).json(results).send();
         }).catch((error) => {
             console.log(error);
-            res.status(400).json("Error obteniendo periodos" + error).send()})
+            return res.status(400).json("Error obteniendo periodos" + error).send()})
     }
 
     public Update = (req: Request, res: Response) => {
         const [error_Arr, academicupdateDTO] = UpdateAcademicTerm.create(req.body);
         if (academicupdateDTO != undefined) {
             new updateAcademicTermUseCase(this.repository).execute(academicupdateDTO)
-            .then((academic)=>{res.status(200).json(academic).send();})
-            .catch((error) => {res.status(400).json("Error actualizando periodos" + error).send()})
+            .then((academic)=>{return res.status(200).json(academic).send();})
+            .catch((error) => {return res.status(400).json("Error actualizando periodos" + error).send()})
         }else{
             console.log(error_Arr);
-            res.status(400).json({ error_Arr }).send();
+            return res.status(400).json({ error_Arr }).send();
         } 
     }
     public PassSemester = (req: Request, res: Response) => {
         const id = +req.params.id;
-        if(id == undefined){res.status(400).json("el id es requerido").send()}
+        if(id == undefined){return res.status(400).json("el id es requerido").send()}
         new PassAcademicTermSemesterUseCase(this.repository).execute(id)
-        .then((academic)=>{res.status(200).json(academic).send();})
-        .catch((error) => {res.status(400).json("Error pasando el semestre del periodo" + error).send()})
+        .then((academic)=>{return res.status(200).json(academic).send();})
+        .catch((error) => {return res.status(400).json("Error pasando el semestre del periodo" + error).send()})
     }
     public Activate = (req: Request, res: Response) => {
         const id = +req.params.id;
-        if(id == undefined){res.status(400).json("el id es requerido").send()}
+        if(id == undefined){return res.status(400).json("el id es requerido").send()}
         new ActivateAcademicTermUseCase(this.repository).execute(id)
-        .then((academic)=>{res.status(200).json(academic).send();})
-        .catch((error) => {res.status(400).json("Error activando el periodo" + error).send()})
+        .then((academic)=>{return res.status(200).json(academic).send();})
+        .catch((error) => {return res.status(400).json("Error activando el periodo" + error).send()})
     }
     public Deactivate = (req: Request, res: Response) => {
         const id = +req.params.id;
         console.log(id)
-        if(id == undefined){res.status(400).json("el id es requerido").send()}
+        if(id == undefined){return res.status(400).json("el id es requerido").send()}
         new EndAcademicTermUseCase(this.repository).execute(id)
-        .then((academic)=>{res.status(200).json(academic).send();})
-        .catch((error) => {res.status(400).json("Error eliminando periodo" + error).send()})
+        .then((academic)=>{return res.status(200).json(academic).send();})
+        .catch((error) => {return res.status(400).json("Error eliminando periodo" + error).send()})
     }
 }
