70a500b79d6189c3e1512f52de1dadb41fa233c9
edduart <edduar13@gmail.com>
2024-09-02 20:52:55 -0400

    fix: add env filepath in seeders of redes data

--- src/seed/data/redes.ts
+++ src/seed/data/redes.ts
@@ -1,68 +1,61 @@
+import { envs } from "../../config/envs";
+
+const filePath = envs.SERVER_ADDRESS;
+
 export const redes = [
   {
     id: 1,
     description: "FACEBOOK",
-    icon: "localhost:3000/images/icons/facebook.svg",
+    icon: filePath + "/images/icons/facebook.svg",
   },
   {
     id: 2,
     description: "X (TWITTER)",
-    icon: "localhost:3000/images/icons/x.svg",
+    icon: filePath + "/images/icons/x.svg",
   },
   {
     id: 3,
     description: "LINKEDIN",
-    icon: "localhost:3000/images/icons/link.svg",
+    icon: filePath + "/images/icons/link.svg",
   },
   {
     id: 4,
     description: "GITHUB",
-    icon: "localhost:3000/images/icons/github.svg",
+    icon: filePath + "/images/icons/github.svg",
   },
   {
     id: 5,
     description: "YOUTUBE",
-    icon: "localhost:3000/images/icons/youtube.svg",
+    icon: filePath + "/images/icons/youtube.svg",
   },
   {
     id: 6,
     description: "TIKTOK",
-    icon: "localhost:3000/images/icons/tiktok.svg",
+    icon: filePath + "/images/icons/tiktok.svg",
   },
   {
     id: 7,
     description: "PINTEREST",
-    icon: "localhost:3000/images/icons/pinte.svg",
+    icon: filePath + "/images/icons/pinte.svg",
   },
   {
     id: 8,
     description: "THREADS",
-    icon: "localhost:3000/images/icons/threa.svg",
+    icon: filePath + "/images/icons/threa.svg",
   },
   {
     id: 9,
     description: "INSTAGRAM",
-    icon: "localhost:3000/images/icons/insta.svg",
+    icon: filePath + "/images/icons/insta.svg",
   },
   {
     id: 10,
     description: "TWITCH",
-    icon: "localhost:3000/images/icons/twit.svg",
+    icon: filePath + "/images/icons/twit.svg",
   },
   {
     id: 11,
     description: "DISCORD",
-    icon: "localhost:3000/images/icons/discord.svg",
+    icon: filePath + "/images/icons/discord.svg",
   },
 ];
-
-
-
-
-
- 
-
-
- 
-
-
