ae136abf18656146ca0a38ddb29ce78ac3d95e18
DemianHoftm <Thegodneko@gmail.com>
2024-09-03 21:46:03 -0400

    ya no se

--- src/presentation/docs/ficha.ts
+++ src/presentation/docs/ficha.ts
@@ -42,7 +42,7 @@ export function BuildFicha(dataCB: (...args: any[]) => void, endCB: (...args: an
         if(data.picture != null){
             const picture = data.picture.split("images");
             const path = ".images" + picture[1];
-            doc.image(path, 442,65,{
+            doc.image("./images/seminarian/V-547896321.jpeg", 442,65,{
                 width:140,
                 height: 140,
                 fit:[160,140],
