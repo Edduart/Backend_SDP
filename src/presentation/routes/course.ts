f060f1a937d005476fed89a931c59f4760c8f18b
edduart <edduar13@gmail.com>
2024-09-05 19:36:06 -0400

    shore: create worker file

--- src/presentation/routes/course.ts
+++ src/presentation/routes/course.ts
@@ -4,8 +4,8 @@ import { CourseDataSourceImpl } from "../../infrastructure";
 import { CourseRepositoryImpl } from "../../infrastructure";
 
 const router = Router();
-const datasource = new CourseDataSourceImpl();
-const courseRepository = new CourseRepositoryImpl(datasource);
+const dataSource = new CourseDataSourceImpl();
+const courseRepository = new CourseRepositoryImpl(dataSource);
 const courseController = new CourseController(courseRepository);
 
 //router.post("/", courseController.createCourse);
