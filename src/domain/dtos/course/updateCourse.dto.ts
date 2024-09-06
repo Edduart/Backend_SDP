fb1be9339db76baba1a3dfa1355d1a279a9f0a00
edduart <edduar13@gmail.com>
2024-09-06 15:22:34 -0400

    feat: add assign instructor to course.

--- src/domain/dtos/course/updateCourse.dto.ts
+++ src/domain/dtos/course/updateCourse.dto.ts
@@ -1,49 +1,21 @@
 export class UpdateCourseDto {
   private constructor(
     public readonly id: number,
-    public readonly stage_id: number,
-    public readonly description: string,
     public readonly instructor_id: string
   ) {}
-
-  get values() {
-    const returnObj: { [key: string]: any } = {};
-
-    if (this.stage_id) returnObj.stage_id = this.stage_id;
-    if (this.description) returnObj.description = this.description;
-    if (this.instructor_id) returnObj.instructor_id = this.instructor_id;
-
-    return returnObj;
-  }
-
   static update(props: { [key: string]: any }): [string?, UpdateCourseDto?] {
-    const { id, stage_id, description, instructor_id } = props;
-
+    const { id, instructor_id } = props;
     if (!id || isNaN(Number(id))) {
       return ["Id must be a number > 0 and integer"];
     }
 
-    if (!stage_id) {
-      return ["Stage ID is required"];
-    } else if (isNaN(Number(stage_id))) {
-      return ["Stage ID must be a valid ID or number"];
+    if (instructor_id !== null) {
+      if (typeof instructor_id !== "string") {
+        return ["Instructor ID must be a string"];
+      } else {
+         if (!/^(V|E)-\d{1,18}$/.test(instructor_id)) return ["Instructor ID must follows this format: V-xxxxxx"];
+      }
     }
-
-    if (!description) {
-      return ["Description is required"];
-    } else if (typeof description !== "string") {
-      return ["Description must be a string"];
-    }
-    let description_u = description.toUpperCase();
-    if (!instructor_id) {
-      return ["Instructor ID is required"];
-    } else if (typeof instructor_id !== "string") {
-      return ["Instructor ID must be a string"];
-    }
-
-    return [
-      undefined,
-      new UpdateCourseDto(id, stage_id, description_u, instructor_id),
-    ];
+    return [undefined, new UpdateCourseDto(id, instructor_id)];
   }
 }
