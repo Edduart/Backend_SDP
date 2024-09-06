a43198387cf92f26a4be08bc47560185d0a8a35d
edduart <edduar13@gmail.com>
2024-09-06 17:36:38 -0400

    fix: add data validations to test score DTOs

--- src/domain/dtos/testScore/getTestScore.dto.ts
+++ src/domain/dtos/testScore/getTestScore.dto.ts
@@ -1,17 +1,31 @@
 export class GetTestScoreDto {
   constructor(public test_id: number, public enrollment_id: number) {}
 
-  static get(props: { [key: string]: any }): [string[]?, GetTestScoreDto?] {
+  static get(props: { [key: string]: any }): [object[]?, GetTestScoreDto?] {
     let { test_id, enrollment_id } = props;
-    let dataErrors: string[] = [];
-
-    // TODO reWork validations
-
-    if (test_id != undefined) test_id = +test_id;
-
-    if (enrollment_id != undefined) enrollment_id = +enrollment_id;
-
-    if (dataErrors.length > 0) return [dataErrors];
+    let validationErrors: ValidationError[] = [];
+    if (test_id != undefined) {
+      test_id = +test_id;
+      if (isNaN(Number(test_id)) || test_id < 0)
+        validationErrors.push({
+          field: "test_id",
+          message: "test_id must be a valid ID",
+        });
+    }
+    if (enrollment_id != undefined) {
+      enrollment_id = +enrollment_id;
+      if (isNaN(Number(enrollment_id)) || enrollment_id < 0)
+        validationErrors.push({
+          field: "enrollment_id",
+          message: "enrollment_id must be a valid ID",
+        });
+    }
+    if (validationErrors.length > 0) return [validationErrors];
     return [undefined, new GetTestScoreDto(test_id, enrollment_id)];
   }
+}
+
+interface ValidationError {
+  field: string;
+  message: string;
 }
--- src/domain/dtos/testScore/updateTestScore.dto.ts
+++ src/domain/dtos/testScore/updateTestScore.dto.ts
@@ -12,8 +12,8 @@ export class UpdateTestScoreDto {
     let validationErrors: ValidationError[] = [];
 
     let allEnrollmentIds: number[] = [];
-    let allTestId: number[] = [];
-    let allScores: number[] = [];
+    let allTestId: any[] = [];
+    let allScores: any[] = [];
 
     tests_score.forEach((test: any) => {
       if (
@@ -21,7 +21,10 @@ export class UpdateTestScoreDto {
         typeof test.enrollment_id == "string" ||
         test.enrollment_id < 0
       )
-        throw "data error in any enrollment_id";
+        validationErrors.push({
+          field: "enrollment_id",
+          message: "data error in any enrollment_id",
+        });
       allEnrollmentIds.push(test.enrollment_id);
       test.test.map((test2: any) => {
         if (
@@ -29,7 +32,10 @@ export class UpdateTestScoreDto {
           typeof test2.test_id == "string" ||
           test2.test_id < 0
         )
-          throw "data error in any test_id";
+          validationErrors.push({
+            field: "test_id",
+            message: "data error in any test_id",
+          });
         allTestId.push(test2.test_id);
         if (
           isNaN(Number(test2.score)) ||
@@ -37,18 +43,23 @@ export class UpdateTestScoreDto {
           test2.score < 0 ||
           test2.score > 20
         )
-          throw "data error in any score";
+          validationErrors.push({
+            field: "score",
+            message: "data error in any score",
+          });
         allScores.push(test2.score);
       });
     });
 
+    if (validationErrors.length > 0) {
+      console.error("CreateEnrollmentDto", { validationErrors });
+      return [validationErrors];
+    }
+
     const enrollmentIds = [...new Set(allEnrollmentIds)];
     const testIds = [...new Set(allTestId)];
 
-    // TODO check all cases validations
-
     console.log({ enrollmentIds, testIds, allScores });
-
     const prepareTestScore = tests_score.reduce(
       (accumulator: any, test: any) => {
         return accumulator.concat(
@@ -61,16 +72,7 @@ export class UpdateTestScoreDto {
       },
       []
     );
-
     console.log({ prepareTestScore });
-    //throw "stop in dto"
-
-    // TODO reWork validations
-
-    if (validationErrors.length > 0) {
-      console.error("CreateEnrollmentDto", { validationErrors });
-      return [validationErrors];
-    }
     return [
       undefined,
       new UpdateTestScoreDto(prepareTestScore, enrollmentIds, testIds),
