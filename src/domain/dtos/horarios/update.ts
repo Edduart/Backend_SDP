af2cc219d1929c10528c11b681d866909a3f60d1
DemianHoftm <Thegodneko@gmail.com>
2024-09-08 11:19:55 -0400

    resultado

--- src/domain/dtos/horarios/update.ts
+++ src/domain/dtos/horarios/update.ts
@@ -1,7 +1,6 @@
 export class UpdateHorario {
     private constructor(
       public readonly id: number,
-      public readonly Curso: string,
       public readonly link?: string
     ) {} 
 
@@ -15,9 +14,6 @@ export class UpdateHorario {
         if (Number.isNaN(id) || !Number.isInteger(id) || id < 0) {
             errorarray.push("ID must be a non-negative integer");
         }
-       /* if(Curso.length = 0 || Curso == undefined){
-            errorarray.push("Curso can not be undefined or empty"); 
-        }else Curso = Curso.toUpperCase();*/
         
         if (errorarray.length > 0) {
             return [errorarray.join(", "), undefined];
